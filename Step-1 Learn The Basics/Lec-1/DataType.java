551b4e72e20ccca90a5e0b5c1fa5be787d313edc
Etteja Hoque <ettejahoque2003@gmail.com>
2025-07-05 12:34:17 +0530

    lec 1

---
+++ Step-1 Learn The Basics/Lec-1/DataType.java
@@ -0,0 +1,17 @@
+public class DataType {
+    public static void main(String[] args) {
+        int a= 12;
+        char c='P';
+        String str = "I am nitu";
+        boolean b = true;
+        // long val =829034758432920134735;
+        // float f = 1.23;
+
+        System.out.println("Integer ::"+a);
+        System.out.println("Character::"+c);
+        System.out.println("String ::"+str);
+        System.out.println("Boolean ::"+b);
+        // System.out.println("Long ::"+val);
+        // System.out.println("Float::"+f);
+    }
+}
